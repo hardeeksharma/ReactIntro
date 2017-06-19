5097860da4d021183ad5c0339d6c5cb2f9372cea
hardeeksharma <hardeek.sharma@gmail.com>
2017-06-19 15:12:26 +0530

    React-Jsx
    Signed-off-by: hardeeksharma <hardeek.sharma@gmail.com>

---
+++ src/react.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// react is a node module
+
+class HelloWorld extends React.Component{
+
+    render(){
+        return (<div>Hello World</div>);
+    }
+}
+
+var elem = <HelloWorld/> // this is equivalent to making object in jsx
+// INStansiation of react component
+
+var node = document.getElementById("app");
+
+// render this component
+ReactDOM.render(elem,node);
+
+
